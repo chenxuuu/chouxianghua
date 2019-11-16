00b0522d0652fa3970d46391094c825ccac1c61e
chenxuuu <lolicon@papapoi.com>
2019-11-16 19:25:51 +0800

    初💩🌸

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # chouxianghua
+
 抽象话生成器，一键生成抽象话
+
+[https://qq.papapoi.com/cxh](https://qq.papapoi.com/cxh)
+
+## 搭建
+
+接口运行环境为openresty，lua脚本在`lua`文件夹中
+
+直接访问`index.html`即可
+
+## 词库
+
+欢迎完善
---
+++ lua/chouxiang.lua
@@ -0,0 +1,35 @@
+--抽象话
+local emoji = require("chouxiang.emoji")
+local pinyin = require("chouxiang.pinyin")
+
+local function utf8ToTable(str)
+    local tab = {}
+    for uchar in string.gmatch(str, "[%z\1-\127\194-\244][\128-\191]*") do
+        tab[#tab + 1] = uchar
+    end
+    return tab
+end
+
+local function getpinyin(s)
+    return table.concat(pinyin(s,true))
+end
+
+return function(s)
+    local data = utf8ToTable(s)
+    local now = 1
+    local result = {}
+    while now <= #data do
+
+        if now < #data and emoji[getpinyin(data[now]..data[now+1])] then
+            table.insert(result,emoji[getpinyin(data[now]..data[now+1])])
+            now = now + 2
+        elseif emoji[getpinyin(data[now])] then
+            table.insert(result,emoji[getpinyin(data[now])])
+            now = now + 1
+        else
+            table.insert(result,data[now])
+            now = now + 1
+        end
+    end
+    return table.concat(result)
+end
